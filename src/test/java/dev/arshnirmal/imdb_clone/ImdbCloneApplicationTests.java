380050dd7ed39bb0181b1fcac0bd23b73ee9a718
arshnirnal <arshnirnal11@gmail.com>
2024-01-22 15:37:44 +0530

    spring initialized

---
+++ src/test/java/dev/arshnirmal/imdb_clone/ImdbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arshnirmal.imdb_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
